b41c653447db3b835f9e0fd50de45b30f9046669
MRzasa97 <m.rzasa.977@gmail.com>
2024-05-16 12:24:45 +0200

    create models for employee and worktimes

---
+++ app/Models/WorkTime.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WorkTime extends Model
+{
+    use HasFactory;
+
+    protected $table = 'work_times';
+    protected $fillable = [
+        'employee_id',
+        'start_time',
+        'end_time',
+        'start_day'
+    ];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+}
