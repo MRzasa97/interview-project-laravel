c455f769095cb3756dfbf99d5d4c68da3ec9d288
MRzasa97 <m.rzasa.977@gmail.com>
2024-05-16 12:51:19 +0200

    middleware to ensure json request

---
+++ app/Http/Middleware/IsJsonRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsJsonRequest
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(!$request->expectsJson())
+        {
+            throw new HttpResponseException(response()->json(['error' => 'Invalid request format. JSON expected!'], 400));
+        }
+
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
+use App\Http\Middleware\IsJsonRequest;
 use App\Http\Middleware\RemoveTrailingSlash;
 
-Route::middleware([RemoveTrailingSlash::class])
+Route::middleware([RemoveTrailingSlash::class, IsJsonRequest::class])
 ->prefix('employees')
 ->group(function() {
     Route::post('/create', [EmployeeController::class, 'create'])->name('employee.create');
